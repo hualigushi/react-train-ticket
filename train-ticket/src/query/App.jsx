10445574ece9f74a21ad7f57c98c89c88c5a8a7b
money <hualigushi@163.com>
2020-09-14 21:39:39 +0800

    feat: 列车查询及筛选

--- train-ticket/src/query/App.jsx
+++ train-ticket/src/query/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import { connect } from "react-redux";
 import URI from "urijs";
 import dayjs from "dayjs";
@@ -8,59 +8,62 @@ import Header from "../common/Header";
 import Nav from "../common/Nav";
 import List from "./List";
 import Bottom from "./Bottom";
+import useNav from "../common/useNav";
+
 import {
-    setFrom,
-    setTo,
-    setDepartDate,
-    setHighSpeed,
-    setSearchParsed,
-    setTrainList,
-    setTicketTypes,
-    setTrainTypes,
-    setDepartStations,
-    setArriveStations,
-    prevDate,
-    nextDate,
-    toggleOrderType,
-    toggleHighSpeed,
-    toggleOnlyTickets,
-    toggleIsFiltersVisible,
-    setCheckedTicketTypes,
-    setCheckedTrainTypes,
-    setCheckedDepartStations,
-    setCheckedArriveStations,
-    setDepartTimeStart,
-    setDepartTimeEnd,
-    setArriveTimeStart,
-    setArriveTimeEnd,
-} from './actions';
+  setFrom,
+  setTo,
+  setDepartDate,
+  setHighSpeed,
+  setSearchParsed,
+  setTrainList,
+  setTicketTypes,
+  setTrainTypes,
+  setDepartStations,
+  setArriveStations,
+  prevDate,
+  nextDate,
+  toggleOrderType,
+  toggleHighSpeed,
+  toggleOnlyTickets,
+  toggleIsFiltersVisible,
+  setCheckedTicketTypes,
+  setCheckedTrainTypes,
+  setCheckedDepartStations,
+  setCheckedArriveStations,
+  setDepartTimeStart,
+  setDepartTimeEnd,
+  setArriveTimeStart,
+  setArriveTimeEnd,
+} from "./actions";
 
 import "./App.css";
+import { bindActionCreators } from "redux";
 function App(props) {
-    const {
-        trainList,
-        from,
-        to,
-        departDate,
-        highSpeed,
-        searchParsed,
-        dispatch,
-        orderType,
-        onlyTickets,
-        isFiltersVisible,
-        ticketTypes,
-        trainTypes,
-        departStations,
-        arriveStations,
-        checkedTicketTypes,
-        checkedTrainTypes,
-        checkedDepartStations,
-        checkedArriveStations,
-        departTimeStart,
-        departTimeEnd,
-        arriveTimeStart,
-        arriveTimeEnd,
-    } = props;
+  const {
+    trainList,
+    from,
+    to,
+    departDate,
+    highSpeed,
+    searchParsed,
+    dispatch,
+    orderType,
+    onlyTickets,
+    isFiltersVisible,
+    ticketTypes,
+    trainTypes,
+    departStations,
+    arriveStations,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+  } = props;
 
   useEffect(() => {
     const queries = URI.parseQuery(window.location.search);
@@ -75,62 +78,51 @@ function App(props) {
 
   useEffect(() => {
     if (!searchParsed) {
-        return;
+      return;
     }
 
-    const url = new URI('/rest/query')
-        .setSearch('from', from)
-        .setSearch('to', to)
-        .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
-        .setSearch('highSpeed', highSpeed)
-        .setSearch('orderType', orderType)
-        .setSearch('onlyTickets', onlyTickets)
-        .setSearch(
-            'checkedTicketTypes',
-            Object.keys(checkedTicketTypes).join()
-        )
-        .setSearch(
-            'checkedTrainTypes',
-            Object.keys(checkedTrainTypes).join()
-        )
-        .setSearch(
-            'checkedDepartStations',
-            Object.keys(checkedDepartStations).join()
-        )
-        .setSearch(
-            'checkedArriveStations',
-            Object.keys(checkedArriveStations).join()
-        )
-        .setSearch('departTimeStart', departTimeStart)
-        .setSearch('departTimeEnd', departTimeEnd)
-        .setSearch('arriveTimeStart', arriveTimeStart)
-        .setSearch('arriveTimeEnd', arriveTimeEnd)
-        .toString();
+    const url = new URI("/rest/query")
+      .setSearch("from", from)
+      .setSearch("to", to)
+      .setSearch("date", dayjs(departDate).format("YYYY-MM-DD"))
+      .setSearch("highSpeed", highSpeed)
+      .setSearch("orderType", orderType)
+      .setSearch("onlyTickets", onlyTickets)
+      .setSearch("checkedTicketTypes", Object.keys(checkedTicketTypes).join())
+      .setSearch("checkedTrainTypes", Object.keys(checkedTrainTypes).join())
+      .setSearch(
+        "checkedDepartStations",
+        Object.keys(checkedDepartStations).join()
+      )
+      .setSearch(
+        "checkedArriveStations",
+        Object.keys(checkedArriveStations).join()
+      )
+      .setSearch("departTimeStart", departTimeStart)
+      .setSearch("departTimeEnd", departTimeEnd)
+      .setSearch("arriveTimeStart", arriveTimeStart)
+      .setSearch("arriveTimeEnd", arriveTimeEnd)
+      .toString();
 
     fetch(url)
-        .then(response => response.json())
-        .then(result => {
-            const {
-                dataMap: {
-                    directTrainInfo: {
-                        trains,
-                        filter: {
-                            ticketType,
-                            trainType,
-                            depStation,
-                            arrStation,
-                        },
-                    },
-                },
-            } = result;
+      .then((response) => response.json())
+      .then((result) => {
+        const {
+          dataMap: {
+            directTrainInfo: {
+              trains,
+              filter: { ticketType, trainType, depStation, arrStation },
+            },
+          },
+        } = result;
 
-            dispatch(setTrainList(trains));
-            dispatch(setTicketTypes(ticketType));
-            dispatch(setTrainTypes(trainType));
-            dispatch(setDepartStations(depStation));
-            dispatch(setArriveStations(arrStation));
-        });
-}, [
+        dispatch(setTrainList(trains));
+        dispatch(setTicketTypes(ticketType));
+        dispatch(setTrainTypes(trainType));
+        dispatch(setDepartStations(depStation));
+        dispatch(setArriveStations(arrStation));
+      });
+  }, [
     from,
     to,
     departDate,
@@ -146,20 +138,74 @@ function App(props) {
     departTimeEnd,
     arriveTimeStart,
     arriveTimeEnd,
-]);
+  ]);
 
   const onBack = useCallback(() => {
     window.history.back();
   }, []);
+
+  const { isPrevDisabled, isNextDisabled, prev, next } = useNav(
+    departDate,
+    dispatch,
+    prevDate,
+    nextDate
+  );
+
+  const bottomCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        toggleOrderType,
+        toggleHighSpeed,
+        toggleOnlyTickets,
+        toggleIsFiltersVisible,
+        setCheckedTicketTypes,
+        setCheckedTrainTypes,
+        setCheckedDepartStations,
+        setCheckedArriveStations,
+        setDepartTimeStart,
+        setDepartTimeEnd,
+        setArriveTimeStart,
+        setArriveTimeEnd,
+      },
+      dispatch
+    );
+  }, []);
+  if (!searchParsed) {
+    return null;
+  }
 
   return (
     <div>
       <div className="header-wrapper">
         <Header title={`${from} - ${to}`} onBack={onBack} />
       </div>
-      <Nav />
-      <List />
-      <Bottom />
+      <Nav
+        date={departDate}
+        isPrevDisabled={isPrevDisabled}
+        isNextDisabled={isNextDisabled}
+        prev={prev}
+        next={next}
+      />
+      <List list={trainList} />
+      <Bottom
+        highSpeed={highSpeed}
+        orderType={orderType}
+        onlyTickets={onlyTickets}
+        isFiltersVisible={isFiltersVisible}
+        ticketTypes={ticketTypes}
+        trainTypes={trainTypes}
+        departStations={departStations}
+        arriveStations={arriveStations}
+        checkedTicketTypes={checkedTicketTypes}
+        checkedTrainTypes={checkedTrainTypes}
+        checkedDepartStations={checkedDepartStations}
+        checkedArriveStations={checkedArriveStations}
+        departTimeStart={departTimeStart}
+        departTimeEnd={departTimeEnd}
+        arriveTimeStart={arriveTimeStart}
+        arriveTimeEnd={arriveTimeEnd}
+        {...bottomCbs}
+      />
     </div>
   );
 }
