98ecab7a262d0e8e1d19f9218d5ade304b040c90
money <hualigushi@163.com>
2020-09-15 00:16:18 +0800

    feat: 车票

--- train-ticket/src/ticket/App.jsx
+++ train-ticket/src/ticket/App.jsx
@@ -1,14 +1,184 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import './App.css'
+import React, { useEffect, useCallback, useMemo, lazy, Suspense } from "react";
+import URI from "urijs";
+import dayjs from "dayjs";
+import { h0 } from "../common/fp";
+import useNav from "../common/useNav";
+import { connect } from "react-redux";
+import Header from "../common/Header";
+import Nav from "../common/Nav";
+import Detail from "../common/Detail";
+import Candidate from "./Candidate";
+import { TrainContext } from "./context";
+import "./App.css";
 
+import {
+  setDepartStation,
+  setArriveStation,
+  setTrainNumber,
+  setDepartDate,
+  setSearchParsed,
+  prevDate,
+  nextDate,
+  setDepartTimeStr,
+  setArriveTimeStr,
+  setArriveDate,
+  setDurationStr,
+  setTickets,
+  toggleIsScheduleVisible,
+} from "./actions";
 
+import { bindActionCreators } from "redux";
+
+const Schedule = lazy(() => import("./Schedule"));
 function App(props) {
-    return (
-        <div>111</div>
-    )
+  const {
+    departDate,
+    arriveDate,
+    departTimeStr,
+    arriveTimeStr,
+    departStation,
+    arriveStation,
+    trainNumber,
+    durationStr,
+    tickets,
+    isScheduleVisible,
+    searchParsed,
+
+    dispatch,
+  } = props;
+
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  useEffect(() => {
+    const quries = URI.parseQuery(window.location.search);
+    const { aStation, dStation, date, trainNumber } = quries;
+
+    dispatch(setDepartStation(dStation));
+    dispatch(setArriveStation(aStation));
+    dispatch(setTrainNumber(trainNumber));
+    dispatch(setDepartDate(h0(dayjs(date).valueOf())));
+
+    dispatch(setSearchParsed(true));
+  }, []);
+
+  useEffect(() => {
+    document.title = trainNumber;
+  }, [trainNumber]);
+
+  useEffect(() => {
+    if (!searchParsed) {
+      return;
+    }
+    const url = new URI("/rest/ticket")
+      .setSearch("date", dayjs(departDate).format("YYYY-MM-DD"))
+      .setSearch("trainNumber", trainNumber)
+      .toString();
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        const { detail, candidates } = result;
+
+        const {
+          departTimeStr,
+          arriveTimeStr,
+          arriveDate,
+          durationStr,
+        } = detail;
+
+        dispatch(setDepartTimeStr(departTimeStr));
+        dispatch(setArriveTimeStr(arriveTimeStr));
+        dispatch(setArriveDate(arriveDate));
+        dispatch(setDurationStr(durationStr));
+        dispatch(setTickets(candidates));
+      });
+  }, [searchParsed, departDate, trainNumber]);
+
+  const { isPrevDisabled, isNextDisabled, prev, next } = useNav(
+    departDate,
+    dispatch,
+    prevDate,
+    nextDate
+  );
+
+  const detailCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        toggleIsScheduleVisible,
+      },
+      dispatch
+    );
+  }, []);
+
+  if (!searchParsed) {
+    return null;
+  }
+
+  return (
+    <div className="app">
+      <div className="header-wrapper">
+        <Header title={trainNumber} onBack={onBack} />
+      </div>
+      <div className="nav-wrapper">
+        <Nav
+          date={departDate}
+          isPrevDisabled={isPrevDisabled}
+          isNextDisabled={isNextDisabled}
+          prev={prev}
+          next={next}
+        />
+      </div>
+      <div className="detail-wrapper">
+        <Detail
+          departDate={departDate}
+          arriveDate={arriveDate}
+          departTimeStr={departTimeStr}
+          arriveTimeStr={arriveTimeStr}
+          trainNumber={trainNumber}
+          departStation={departStation}
+          arriveStation={arriveStation}
+          durationStr={durationStr}
+        >
+          <span className="left"></span>
+          <span
+            className="schedule"
+            onClick={() => detailCbs.toggleIsScheduleVisible()}
+          >
+            时刻表
+          </span>
+          <span className="right"></span>
+        </Detail>
+      </div>
+      {isScheduleVisible && (
+        <div
+          className="mask"
+          onClick={() => dispatch(toggleIsScheduleVisible())}
+        >
+          <Suspense fallback={<div>loading</div>}>
+            <Schedule
+              date={departDate}
+              trainNumber={trainNumber}
+              departStation={departStation}
+              arriveStation={arriveStation}
+            />
+          </Suspense>
+        </div>
+      )}
+      <TrainContext.Provider value={{
+        trainNumber, departStation,arriveStation,departDate
+      }}>
+        <Candidate tickets={tickets} />
+      </TrainContext.Provider>
+    </div>
+  );
 }
 export default connect(
-    function mapStateToProps(state) {},
-    function mapDispatchToProps(dispatch) {}
-)(App)
+  function mapStateToProps(state) {
+    return state;
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch };
+  }
+)(App);
